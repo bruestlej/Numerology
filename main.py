711151cd53d433b57fcc11b98726d0c8e0119986
James Bruestle <131274685+bruestlej@users.noreply.github.com>
2025-07-03 14:02:41 +0000

    Update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 class Numerology:
-    def _init_(self, sName, sDOB):
+    def __init__(self, sName, sDOB):
         self.__sName = sName
         self.__sDOB = sDOB
-        
 
     def getName(self):
         return self.__sName
@@ -11,48 +10,47 @@ class Numerology:
         return self.__sDOB
 
     def getAttitude(self):
-      self.sAttitudeNum = self.__reduceNum(int(self.__sDOB[0:2] + self.__sDOB[3:5]))
-      return self.__sAttitudeNum
+        attitudeNum = self.__reduceNum(int(self.__sDOB[0:2] + self.__sDOB[3:5]))
+        return attitudeNum
 
     def getBirthday(self):
-      self.sBirthdayNum = self.__reduceNum(int(sDOB[3:5]))
-      return self.__sBirthdayNum
+        birthdayNum = self.__reduceNum(int(self.__sDOB[3:5]))
+        return birthdayNum
 
     def getLifePath(self):
-        self.sLifePathNum = self.__reduceNum(int(self.__sDOB[0:2] + self.__sDOB[3:5] + self.__sDOB[6:]))
-        return self.__sLifePathNum
-
-    def getPersonality(self):
-        lstVowels = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u']
-
-        lstVowel2Num = []
-        lstCon2Num = []
-      
-        for lstVowels not in sName:
-
-
-    def getPowerName(self):
-        sPowerNum = sSoulNum + sPersonalityNum
+        lifePathNum = self.__reduceNum(int(self.__sDOB[0:2] + self.__sDOB[3:5] + self.__sDOB[6:]))
+        return lifePathNum
 
     def getSoul(self):
-        for lstVowels in sName:
+        vowels = 'aeiouAEIOU'
+        soulTotal = 0
+        for char in self.__sName:
+            if char in vowels:
+                soulTotal += self.__convertCharToInt(char)
+        return self.__reduceNum(soulTotal)
 
+    def getPersonality(self):
+        vowels = 'aeiouAEIOU'
+        personalityTotal = 0
+        for char in self.__sName:
+            if char.isalpha() and char not in vowels:
+                personalityTotal += self.__convertCharToInt(char)
+        return self.__reduceNum(personalityTotal)
 
+    def getPowerName(self):
+        # Power number is typically: Soul Number + Personality Number
+        soulNum = self.getSoul()
+        personalityNum = self.getPersonality()
+        return self.__reduceNum(soulNum + personalityNum)
 
     def __reduceNum(self, iNum):
-
-        iMultiDigit = int(iNum)
-
-        while iMultiDigit > 9:
-            iMultiDigit = iMultiDigit // 10 + iMultiDigit % 10
-        return iMultiDigit
+        # Reduce until single-digit unless it's a Master Number (optional)
+        while iNum > 9:
+            digits = [int(d) for d in str(iNum)]
+            iNum = sum(digits)
+        return iNum
 
     def __convertCharToInt(self, sCharacter):
-
-      iCharacterToNumberValue = 0
-
-      if sCharacter.isalpha():
-
-        iCharacterToNumberValue = (( ord(sCharacter.upper() ) - 65) % 9 + 1)
-
-      return iCharacterToNumberValue
+        if sCharacter.isalpha():
+            return ((ord(sCharacter.upper()) - 65) % 9 + 1)
+        return 0
